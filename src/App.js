c551f657997fa0683b795606b0c244fa9b61d59e
armandk <armand_komenan@yahoo.com>
2021-02-03 14:47:25 -0500

    aa

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React to react again
+          Learn React to react not 
         </a>
 
       </header>
